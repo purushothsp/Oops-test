a0f882d9f32bcf32eeb566dbb35c7a7fad11509f
Purush_K <purushot052002@gmail.com>
2023-05-16 17:45:14 +0530

    15/05/2023

---
+++ Multi_level_inheritance/multilevel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace multilevel
+{
+    class karam
+    {
+        public void val()
+        {
+            Console.WriteLine("Vegetable Pakoda");
+        }
+    }
+    class sweet : karam
+
+    {
+        public void swt()
+        {
+            Console.WriteLine("MothiLaddu");
+        }
+    }
+    class even_snacks : sweet
+    {
+        public void even()
+        {
+            Console.WriteLine("Ragi Cookies");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            even_snacks snacks = new even_snacks();
+
+            snacks.val(); 
+            snacks.swt();
+            snacks.even();
+        }
+    }
+}
