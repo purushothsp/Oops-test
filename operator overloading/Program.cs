31c095226827d5a8ec5a2fb4ed40ad2b5ba7703c
Purush_K <purushot052002@gmail.com>
2023-05-17 12:15:10 +0530

    operator_overloading

---
+++ operator overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace operator_overloading
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        // Constructor
+        public Food(string name)
+        {
+            Name = name;
+        }
+        // Overloading the + operator to combine two Food objects
+        public static Food operator +(Food food1, Food food2)
+        {
+            string Name = $"{food1.Name} {food2.Name} Combo";
+            return new Food(Name);
+        }
+    }
+    class pro
+    {
+        static void Main(string[] args)
+        {
+            Food pizza = new Food("Pizza");
+            Food burger = new Food("Burger");
+            Food combo = pizza + burger;
+            Console.WriteLine(combo.Name);
+        }
+    }
+}
